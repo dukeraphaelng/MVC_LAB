1fcdcf427709f1f480ebd708c5e91146b01e729d
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-04-03 10:01:04 +1100

    updated the boilerplate

--- PostController.rb
+++ PostController.rb
@@ -2,7 +2,19 @@ require_relative("PostModel")
 require_relative("PostView")
 
 class PostController
-    def run
-        puts "I am running"
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def destroy
+
     end
 end
--- index.rb
+++ index.rb
@@ -1,3 +1,30 @@
 require_relative("PostController")
+controller = PostController.new
 
-PostController.run
+while true
+    puts "What would you like to do?"
+    puts "1 - View a post"
+    puts "2 - Create a new post"
+    puts "3 - Edit a post"
+    puts "4 - Delete a post"
+    puts "5 - Exit"
+    
+    choice = gets.chomp.to_i
+    
+    case choice
+    when 1:
+        controller.show
+    when 2:
+        controller.new
+    when 3:
+        controlled.edit
+    when 4:
+        controller.destroy
+    when 5:
+        break
+    else
+        puts "Please pick a valid choice"
+    end
+end
+
+puts "Goodbye"
